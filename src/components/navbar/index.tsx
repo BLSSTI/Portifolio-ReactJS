61a9f5d495809e1f86a402aad94b48721c06eaa2
Bruno Luis <bruno-aqv@outlook.com>
2021-02-19 22:49:45 -0300

    Adicionado Navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import '../../App';
+import {Nav} from './style';
+
+const NavBar: React.FC = () => (
+  <>
+  <div>
+    <Nav>
+    <nav>
+      <ul>
+        <li>PROJETOS</li>
+        <li>SOBRE</li>
+        <li>CONTATO</li>
+      </ul>
+    </nav>
+    </Nav>
+  </div>
+
+  </>
+)
+
+export default NavBar;
