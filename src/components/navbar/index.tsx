c5dc38dd81fcfeab24e6739c4bb6ec99b6cc2331
Bruno Luis <bruno-aqv@outlook.com>
2021-02-19 23:47:32 -0300

    Navbar estilizado

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,23 +1,15 @@
-
-import React from 'react';
-import '../../App';
-import {Nav} from './style';
+import React from "react";
+import "../../App";
+import { Nav } from "./style";
 
 const NavBar: React.FC = () => (
   <>
-  <div>
     <Nav>
-    <nav>
-      <ul>
-        <li>PROJETOS</li>
-        <li>SOBRE</li>
-        <li>CONTATO</li>
-      </ul>
-    </nav>
+      <a>PROJETOS</a>
+      <a>SOBRE</a>
+      <a>CONTATO</a>
     </Nav>
-  </div>
-
   </>
-)
+);
 
 export default NavBar;
--- src/components/navbar/style.tsx
+++ src/components/navbar/style.tsx
@@ -1,6 +1,23 @@
 import styled  from "styled-components"
 
-export const Nav =  styled.nav`
-  background-color:black;
+export const Nav =  styled.div`
+  background-color: #151616;
+
+a{
+  color: #4CA7A1;
+  list-style-type: none;
+  display: inline-flex;
+  justify-content: space-around;
+  font-size: 36px;
+  padding: 14px 16px;
+  cursor: pointer;
+ 
+  }
+a:hover{
+  background-color:#cfebf8;
+  transition:1s;
+  }
+
+  
 `;
 
